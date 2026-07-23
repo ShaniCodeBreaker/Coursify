0b1fcc13bbf48ced29d9d6a06f26a9ca8fd728ff
ShaniCodeBreaker <shani@mirafra.com>
2026-07-23 11:56:50 +0530

    Add files via upload

---
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <h2>Mirafra Course</h2>
+
+      <ul>
+        <li>Admin Panel</li>
+        <li>Dashboard</li>
+        <li>Courses</li>
+        <li>Lesson</li>
+        <li>Quiz</li>
+        <li>Certificates</li>
+        <li>Profile</li>
+        <li>Login</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
